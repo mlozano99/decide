478bba4f228f6e7529156e200d743c256df100c3
jose <jose@diazcantero.es>
2012-12-06 19:09:43 +0100

    Funcionalidad básica que te da las coordenadas latitud y longitud (float) en grados.

--- decide/src/es/dabdm/decide/MainActivity.java
+++ decide/src/es/dabdm/decide/MainActivity.java
@@ -1,15 +1,33 @@
 package es.dabdm.decide;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.l_main_activity);
+        
+        Button botonPlay = (Button) findViewById(R.id.button1);
+        botonPlay.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(
+						new Intent(MainActivity.this,
+								LocationActivity.class));
+			}
+		});        
+        
+        
     }
 
     @Override
