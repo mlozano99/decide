da67e143844abbc053c79cd256fd516635fd31b6
jose <jose@diazcantero.es>
2012-12-09 12:23:12 +0100

    Esqueleto básico de la aplicación Lista de activities a desarrollar

---
+++ decide/src/es/dabdm/decide/ComunidesActivity.java
@@ -0,0 +1,11 @@
+package es.dabdm.decide;
+
+
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class ComunidesActivity extends BaseActivity {
+
+}
