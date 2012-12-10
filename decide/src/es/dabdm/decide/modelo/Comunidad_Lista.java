c0353eb6a93a21aa243b71569bac25a402fde5f6
jose <jose@diazcantero.es>
2012-12-10 09:00:03 +0100

    Lista de comunidades

---
+++ decide/src/es/dabdm/decide/modelo/Comunidad_Lista.java
@@ -0,0 +1,14 @@
+package es.dabdm.decide.modelo;
+import java.util.List;
+
+public class Comunidad_Lista {
+	private List<Comunidad> Comunidades;
+	
+	public List<Comunidad> getComunidad() {
+		return Comunidades;
+	}
+	public void setComunidad(List<Comunidad> Comunidades) {
+		this.Comunidades=Comunidades;
+	}
+}
+
