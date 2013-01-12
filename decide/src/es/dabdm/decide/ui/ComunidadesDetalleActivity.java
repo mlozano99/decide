d77ffd92569088c8471f8bea9ee722d3bee9d7b2
jose <jose@diazcantero.es>
2013-01-12 13:37:14 +0100

    Detalle comunidad. v1.1

--- decide/src/es/dabdm/decide/ui/ComunidadesDetalleActivity.java
+++ decide/src/es/dabdm/decide/ui/ComunidadesDetalleActivity.java
@@ -2,18 +2,28 @@ package es.dabdm.decide.ui;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONStringer;
+
+
 
 import es.dabdm.decide.R;
 import es.dabdm.decide.modelo.Comunidad;
 import es.dabdm.decide.modelo.ListaComunidades;
+import es.dabdm.decide.util.MCuenta;
 import es.dabdm.decide.util.Repositorio;
 import android.content.Context;
 import android.content.Intent;
@@ -33,67 +43,72 @@ public class ComunidadesDetalleActivity extends BaseActivity {
 	SharedPreferences preferences ;
 	boolean usu_iden;
 	String email;
+	MCuenta cuenta;
+	Comunidad comu;
+	Button b1,b2;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.l_detallcomunidad);
 		Intent i = getIntent();
-		final Comunidad comu = (Comunidad) i.getSerializableExtra("Comunidad");	
+		comu = (Comunidad) i.getSerializableExtra("Comunidad");	
+		cuenta = MCuenta.getMC(this);
 		
-	
-		preferences = getSharedPreferences(Repositorio.PerfilPref, Context.MODE_PRIVATE);
-        usu_iden = (preferences.getInt("identificado", 0)==1) ? true:false ; 
-        
-        
         
         /* **************************************************** */
         /* Comprobaciones de usuario identificado */
-        	email = preferences.getString("email","");
-		
-		
-		
-		
+
 		
 		TextView t = (TextView) findViewById(R.id.tv_DCnombre);		
 		t.setText( comu.getNombre() + comu.getIdComunidad() );
 		
 		TextView t2 = (TextView) findViewById(R.id.tv_DCdescripcion);		
 		t2.setText( comu.getDescripcion());
-		
-		Button b1 = (Button) findViewById(R.id.b_suscribir);
-		Button b2 = (Button) findViewById(R.id.b_desuscribir);
 
-		b1.setVisibility( comu.getSuscrito().equalsIgnoreCase("N") ? View.VISIBLE :View.INVISIBLE );
-		b2.setVisibility( comu.getSuscrito().equalsIgnoreCase("N") ? View.INVISIBLE :View.VISIBLE );
-		
-		
-	    b1.setOnClickListener(new View.OnClickListener() 
-		{	
-	    	//String email = "jdiazcan@email.es"; 
-	    	
-		 	Integer idComunidad = comu.getIdComunidad();
-		 	Object[] parametros =  {email, idComunidad} ; 
-	    	
-		 	@Override
-			public void onClick(View v) 
-	    	{
-	    		new EnviarSuscripcionUsuarioComunidad().execute(parametros);
-			}	
-		});
-	    
-	    b2.setOnClickListener(new View.OnClickListener() 
-		{			
-	    	@Override
-			public void onClick(View v) 
-			{
+		if (cuenta.usu_identificado()){
 
-			}	
-		});
+			b1 = (Button) findViewById(R.id.b_suscribir);
+			b2 = (Button) findViewById(R.id.b_desuscribir);
+			
+			boolean suscrita=false;			
+			String CoSuscrita = cuenta.getString("CoSuscritas");			
+			suscrita = CoSuscrita.contains(comu.getIdComunidad().toString()) ? true : false ;  
+			
+			b1.setVisibility( suscrita ? View.GONE :View.VISIBLE );
+			b2.setVisibility( suscrita ? View.VISIBLE :View.GONE );
+			
 
-	    
+		}
 		
 		Log.i("A:", "llega extras vacioas ");
+		
+	}
+	
+	/* ************************************************* */
+	/* Botones del layout */
+	/* ************************************************* */
+	/* Enviar suscripción de un usuario a una comunidad */
+	public void suscribirseComunidad(View view){
+		
+    	String email = cuenta.getString("email"); 
+	 	Integer idComunidad = comu.getIdComunidad();
+	 	Object[] parametros =  {email, idComunidad} ; 
+    	
+	 	new EnviarSuscripcionUsuarioComunidad().execute(parametros);
+		
+	}
+	
+	/* Enviar desuscripción de un usuario a una comunidad */	
+	public void desuscribirseComunidad(View view){
+		
+    	String email = cuenta.getString("email"); 
+	 	Integer idComunidad = comu.getIdComunidad();
+	 	Object[] parametros =  {email, idComunidad} ; 
+    	
+	 	new EnviarDesuscripcionUsuarioComunidad().execute(parametros);
 		
 	}
 	
@@ -101,7 +116,7 @@ public class ComunidadesDetalleActivity extends BaseActivity {
 	
 	
 	/* **************************************************************** */
-    /* PUT con una suscripcion del usuario en una comunidad				*/   
+    /* PUT con una SUScripcion del usuario en una comunidad				*/   
 	/* **************************************************************** */
 	private class EnviarSuscripcionUsuarioComunidad extends AsyncTask<Object, Void, Object[]> {
     	
@@ -124,7 +139,7 @@ public class ComunidadesDetalleActivity extends BaseActivity {
     		 	    			  
 			try {
 						
-				android.util.Log.i("EnviarSuscripcionUsuarioComunidad","Peticion a " + Repositorio.URLsuscripciones);
+				android.util.Log.i("EnviarSuscripcionUsuarioComunidad","Peticion a " + Repositorio.URLsuscripciones );
 				
 				request = new HttpPut(Repositorio.URLsuscripciones);
 				request.setEntity(new UrlEncodedFormEntity(pares));				
@@ -141,34 +156,129 @@ public class ComunidadesDetalleActivity extends BaseActivity {
 		protected void onPostExecute(Object[] result) {
 			// TODO Auto-generated method stub
 
-    		String email = result[0].toString();
+    		String email = result[0].toString();    		
+    		String comu_suscritas = cuenta.getString("CoSuscritas");
+    		comu_suscritas= comu_suscritas.length()==0 ? comu.getIdComunidad().toString() : "," + comu.getIdComunidad().toString() ;
+   		
+    		cuenta.putString("CoSuscritas", comu_suscritas);
+    	
+    		//boolean suscrita = comu_suscritas.contains(comu.getIdComunidad().toString());
+			b1.setVisibility(View.GONE);
+			b2.setVisibility(View.VISIBLE);
+    		
+    		
+    		
+    		Toast.makeText(getApplicationContext(),"Suscripción realizada para " + result[0] + " "+ result[1] ,Toast.LENGTH_SHORT).show();
+    		super.onPostExecute(result);
+		}
+    	
+      	
+	}
+	
+	
+	
+	//EnviarDesuscripcionUsuarioComunidad
+
+	
+	/* **************************************************************** */
+    /* DELETE -- Elimina una suscripción de un usuario a una comunidad  */   
+	/* **************************************************************** */
+	private class EnviarDesuscripcionUsuarioComunidad extends AsyncTask<Object, Void, Object[]> {
+    	
+    	@Override
+    	protected void onPreExecute() {    
+    		super.onPreExecute();
+    	}
+
+    	@Override
+    	protected Object[] doInBackground(Object...params) {
+            		
+    		HttpClient client = new DefaultHttpClient();
+    		HttpDelete request = null;
+    		List<NameValuePair> pares = new ArrayList<NameValuePair>();
+    		
+    	    String email = (String) params[0];
+    	    Integer idComunidad = (Integer) params[1];
+    		pares.add( new BasicNameValuePair("email", email ) );
+    		pares.add( new BasicNameValuePair("idComunidad",  Integer.toString(idComunidad) ) );
+    		 	    			  
+			try {
+						
+				android.util.Log.i("EnviarSuscripcionUsuarioComunidad","Peticion a " + Repositorio.URLsuscripciones 
+						+ "?" + URLEncodedUtils.format(pares, "utf-8"));
+				request = new HttpDelete(Repositorio.URLsuscripciones);
+				//request.setEntity(new UrlEncodedFormEntity(pares));			//+ "?" + URLEncodedUtils.format(pares, "utf-8"));
+				client.execute(request);								
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return params;
+    	}
+    	
+
+    	/* Borrar el id de comunidad del string de comunidades suscritas que tenemos en el perfil
+    	 */
+    	@Override
+		protected void onPostExecute(Object[] result) {
+			// TODO Auto-generated method stub
+    		
+    		//String email = result[0].toString();    		
+    		String comu_suscritas = cuenta.getString("CoSuscritas");
+    		
+    		List<String> ls_comu = new ArrayList<String>(Arrays.asList(comu_suscritas.split(",")));
+    		ls_comu.remove(comu.getIdComunidad().toString());
+    		
+    		StringBuilder sal= new StringBuilder();
+    		for (String s: ls_comu) {
+    			if (ls_comu.size()==1) sal.append(s);
+    			else ls_comu.add("," + s);
+    		}
+    		cuenta.putString("CoSuscritas",sal.toString());
+    		
+    		
+    		/* Activa la suscripcion */
+    		boolean suscrita = ls_comu.contains(comu.getIdComunidad().toString());
+			b1.setVisibility(View.VISIBLE);
+			b2.setVisibility(View.GONE);
+    		
     		
     		Toast.makeText(getApplicationContext(),"Suscripción realizada para " + email,Toast.LENGTH_SHORT).show();
     		super.onPostExecute(result);
 		}
     	
-    	
-    	/*
-    	@Override
-		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
-
-    		
-    		Toast.makeText(getApplicationContext(),"Suscripción realizada para " + result,Toast.LENGTH_SHORT).show();
-    		super.onPostExecute(result);
-		}*/
       	
 	}
 
 	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
 }
+
+
+/*b1.setOnClickListener(new View.OnClickListener() 
+{	
+	String email = cuenta.getString("email"); 
+ 	Integer idComunidad = comu.getIdComunidad();
+ 	Object[] parametros =  {email, idComunidad} ; 
+	
+ 	@Override
+	public void onClick(View v) 
+	{
+		new EnviarSuscripcionUsuarioComunidad().execute(parametros);
+	}	
+});
+
+
+b2.setOnClickListener(new View.OnClickListener() 
+{			
+	
+	String email = cuenta.getString("email"); 
+ 	Integer idComunidad = comu.getIdComunidad();
+ 	Object[] parametros =  {email, idComunidad} ; 
+	
+	
+	@Override
+	public void onClick(View v) 
+	{
+		new EnviarDesuscripcionUsuarioComunidad().execute(parametros);
+	}	
+});*/
